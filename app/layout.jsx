b590d148bd94e225d99d614bfab85ede0f6b6a9a
Archange16 <asathoud16@gmail.com>
2025-05-30 09:03:38 +0100

    acceuil

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,45 +20,12 @@ export default function RootLayout({ children }) {
         }
     }, []);
 
-   /*  useEffect(() => {
-        // Initialisation du Pixel Facebook
-        if (typeof window !== 'undefined') {
-          window.fbq = window.fbq || function(){(window.fbq.q=window.fbq.q||[]).push(arguments)};
-          window.fbq('init', process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID);
-          
-          // Track initial pageview
-          window.fbq('track', 'PageView');
-        }
-      }, []); */
-      
-
     return (
         <html lang="en">
             <head>
                 <script
                     src="https://checkout.revolut.com/checkout.js" // Assurez-vous que ce lien est correct
                     async
-                />
-                <script src="//app.quickreviewer.com/proof/webproof/qrv2.js" defer></script>
-                <link rel='icon' type='image/png' href='../favicon.ico' />
-
-                <script
-                    id="meta-pixel"
-                    strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                        !function(f,b,e,v,n,t,s)
-                        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                        n.queue=[];t=b.createElement(e);t.async=!0;
-                        t.src=v;s=b.getElementsByTagName(e)[0];
-                        s.parentNode.insertBefore(t,s)}(window,document,'script',
-                        'https://connect.facebook.net/en_US/fbevents.js');
-                        fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}'); // Votre Pixel ID
-                        fbq('track', 'PageView');
-                        `,
-                    }}
                 />
             </head>
             <body>
